8377f79fada9bc1b0386cd3f9cb293e574715eb7
Arman Moalemi <124406686+ItsTheLOoLOo@users.noreply.github.com>
2023-04-24 23:45:22 +0200

    Add files via upload

---
+++ Payment/Payment.js
@@ -0,0 +1,38 @@
+function myFunction1() {
+    document.getElementById("container-6").style.height = "320px";
+    document.getElementById("container-7").style.height = "60px";
+    document.getElementById("container-8").style.height = "60px";
+}
+function myFunction2() {
+    document.getElementById("container-6").style.height = "60px";
+    document.getElementById("container-7").style.height = "320px";
+    document.getElementById("container-8").style.height = "60px";
+}
+function myFunction3() {
+    document.getElementById("container-6").style.height = "60px";
+    document.getElementById("container-7").style.height = "60px";
+    document.getElementById("container-8").style.height = "320px";
+}
+
+function myRes1() {
+    document.getElementById("container-6").style.height = "546px";
+    document.getElementById("container-7").style.height = "60px";
+    document.getElementById("container-8").style.height = "60px";
+}
+function myRes2() {
+    document.getElementById("container-6").style.height = "60px";
+    document.getElementById("container-7").style.height = "546px";
+    document.getElementById("container-8").style.height = "60px";
+}
+function myRes3() {
+    document.getElementById("container-6").style.height = "60px";
+    document.getElementById("container-7").style.height = "60px";
+    document.getElementById("container-8").style.height = "546px";
+}
+
+function sendAlert() {
+    alert("Your response has been recorded. (Not actually, this is just a demo!)");
+}
+function getPromoCode() {
+    alert("Your promo code is not valid.");
+}
