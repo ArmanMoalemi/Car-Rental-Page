4da6e9f5d3a3f1a28c09d7efebbaf308eac66d44
Arman Moalemi <124406686+ItsTheLOoLOo@users.noreply.github.com>
2023-04-24 23:46:07 +0200

    Add files via upload

---
+++ Cars/Preview.js
@@ -0,0 +1,48 @@
+function mySlider1() {
+    document.getElementById("slide-2").style.zIndex = "-1";
+    document.getElementById("slide-1").style.zIndex = "1";
+    document.getElementById("slide-3").style.zIndex = "-1";
+    document.getElementById("selectedCar").style.width = "132px";
+    document.getElementById("selectedCar").style.height = "108px";
+    document.getElementById("carPicsSelected1").style.border = "2px solid rgba(53, 99, 233, 1)";
+    document.getElementById("carPicsSelected1").style.borderRadius = "10px";
+
+    document.getElementById("insideCar2").style.width = "152.77px";
+    document.getElementById("insideCar2").style.height = "128px";
+    document.getElementById("carPicsSelected2").style.border = "none";
+    document.getElementById("insideCar3").style.width = "152.77px";
+    document.getElementById("insideCar3").style.height = "128px";
+    document.getElementById("carPicsSelected3").style.border = "none";
+}
+function mySlider2() {
+    document.getElementById("slide-2").style.zIndex = "1";
+    document.getElementById("slide-1").style.zIndex = "-1";
+    document.getElementById("slide-3").style.zIndex = "-1";
+    document.getElementById("insideCar2").style.width = "132px";
+    document.getElementById("insideCar2").style.height = "108px";
+    document.getElementById("carPicsSelected2").style.border = "2px solid rgba(53, 99, 233, 1)";
+    document.getElementById("carPicsSelected2").style.borderRadius = "10px";
+
+    document.getElementById("selectedCar").style.width = "152.77px";
+    document.getElementById("selectedCar").style.height = "128px";
+    document.getElementById("carPicsSelected1").style.border = "none";
+    document.getElementById("insideCar3").style.width = "152.77px";
+    document.getElementById("insideCar3").style.height = "128px";
+    document.getElementById("carPicsSelected3").style.border = "none";
+}
+function mySlider3() {
+    document.getElementById("slide-2").style.zIndex = "-1";
+    document.getElementById("slide-1").style.zIndex = "-1";
+    document.getElementById("slide-3").style.zIndex = "1";
+    document.getElementById("insideCar3").style.width = "132px";
+    document.getElementById("insideCar3").style.height = "108px";
+    document.getElementById("carPicsSelected3").style.border = "2px solid rgba(53, 99, 233, 1)";
+    document.getElementById("carPicsSelected3").style.borderRadius = "10px";
+
+    document.getElementById("selectedCar").style.width = "152.77px";
+    document.getElementById("selectedCar").style.height = "128px";
+    document.getElementById("carPicsSelected1").style.border = "none";
+    document.getElementById("insideCar2").style.width = "152.77px";
+    document.getElementById("insideCar2").style.height = "128px";
+    document.getElementById("carPicsSelected2").style.border = "none";
+}
