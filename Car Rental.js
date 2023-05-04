ce7d1e1b7e6cf09407a07be839c4e414ff295ae3
Arman Moalemi <armanmoalemi81@yahoo.com>
2023-05-05 01:04:11 +0300

    Added like button (favorites)
    Will add :
    - Favorite list
    - Login / Signup
    - User panel
    As soon as possible.

---
+++ Car Rental.js
@@ -0,0 +1,111 @@
+let btnConst1 = document.getElementById("favorite1");
+function favorited1() {
+if (btnConst1.style.color == "lightgray") {
+    btnConst1.style.color = "red";
+    // alert("added to your favorte lits!");
+} else {
+    btnConst1.style.color = "lightgray";
+}
+}
+let btnConst2 = document.getElementById("favorite2");
+function favorited2() {
+if (btnConst2.style.color == "lightgray") {
+    btnConst2.style.color = "red";
+} else {
+    btnConst2.style.color = "lightgray";
+}
+}
+let btnConst3 = document.getElementById("favorite3");
+function favorited3() {
+if (btnConst3.style.color == "lightgray") {
+    btnConst3.style.color = "red";
+} else {
+    btnConst3.style.color = "lightgray";
+}
+}
+let btnConst4 = document.getElementById("favorite4");
+function favorited4() {
+if (btnConst4.style.color == "lightgray") {
+    btnConst4.style.color = "red";
+} else {
+    btnConst4.style.color = "lightgray";
+}
+}
+let btnConst5 = document.getElementById("favorite5");
+function favorited5() {
+if (btnConst5.style.color == "lightgray") {
+    btnConst5.style.color = "red";
+} else {
+    btnConst5.style.color = "lightgray";
+}
+}
+let btnConst6 = document.getElementById("favorite6");
+function favorited6() {
+if (btnConst6.style.color == "lightgray") {
+    btnConst6.style.color = "red";
+} else {
+    btnConst6.style.color = "lightgray";
+}
+}
+let btnConst7 = document.getElementById("favorite7");
+function favorited7() {
+if (btnConst7.style.color == "lightgray") {
+    btnConst7.style.color = "red";
+} else {
+    btnConst7.style.color = "lightgray";
+}
+}
+let btnConst8 = document.getElementById("favorite8");
+function favorited8() {
+if (btnConst8.style.color == "lightgray") {
+    btnConst8.style.color = "red";
+} else {
+    btnConst8.style.color = "lightgray";
+}
+}
+let btnConst9 = document.getElementById("favorite9");
+function favorited9() {
+if (btnConst9.style.color == "lightgray") {
+    btnConst9.style.color = "red";
+} else {
+    btnConst9.style.color = "lightgray";
+}
+}
+let btnConst10 = document.getElementById("favorite10");
+function favorited10() {
+if (btnConst10.style.color == "lightgray") {
+    btnConst10.style.color = "red";
+} else {
+    btnConst10.style.color = "lightgray";
+}
+}
+let btnConst11 = document.getElementById("favorite11");
+function favorited11() {
+if (btnConst11.style.color == "lightgray") {
+    btnConst11.style.color = "red";
+} else {
+    btnConst11.style.color = "lightgray";
+}
+}
+let btnConst12 = document.getElementById("favorite12");
+function favorited12() {
+if (btnConst12.style.color == "lightgray") {
+    btnConst12.style.color = "red";
+} else {
+    btnConst12.style.color = "lightgray";
+}
+}
+
+// let btnlet = document.getElementById("favorite");
+// let colorFinder = btnlet.style.color;
+// let colorShower = "gray";
+// function favorited1() {
+//     switch (colorShower) {
+//         case "gray" : {
+//             btnlet.style.color = "red";
+//             break;
+//         }
+//         default :
+//         btnlet.style.color = "gray";
+//     } 
+// }
